8669a24b1f3b539a3d8e9edd8c7c7e1dbf1ffce9
MikeDetroit <mikedetroit@gmail.com>
2018-05-07 08:51:18 -0400

    Lab 9 partial

---
+++ src/com/circle/gc/Circle.java
@@ -0,0 +1,5 @@
+package com.circle.gc;
+
+public class Circle {
+
+}
---
+++ src/com/circle/gc/CircleTester.java
@@ -0,0 +1,31 @@
+package com.circle.gc;
+import java.util.Scanner;
+
+
+
+public class CircleTester {
+
+	public static void main(String[] args) {
+		
+		Circle circleApp = new Circle();
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Welcome to the Circle tester:");
+		System.out.println();
+				
+		Validator.getInt(scan, "Please enter a radius: ");
+		
+		
+
+	}
+	
+	public static int CircleApp() {
+		return 0;
+	}
+
+}
+
+//  I attempted to do this lab  on my own and ran out of time to complete
+//  I feel like I am making good progress starting to understand the coding process
+//  I think I should be able to complete future labs successfully and will seek the help that I need.
